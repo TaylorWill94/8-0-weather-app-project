c55a81d437bb92b4e072cbdef2409ee0ce3cb720
Taylor W <taylorwilliams@pursuit.org>
2021-10-12 01:04:00 -0400

    Some changes made to js file

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
-document.querySelector('form')
+document.querySelector('.weather')
 .addEventListener('submit', (event) => {
     event.preventDefault();
-}
-)
+    const userInput = event.target.weather;
+
+
+fetch(`https://wttr.in/${userInput}?format=j1`)
+.then((response) => response.json())
+.then((weather) => {
+
+        const cityInfo = weather.filter((forecast) => forecast.FeelsLikeF)
+        //console.log(result);
+        const displayArea = document.querySelector('.display')
+        displayArea.textContent = cityInfo;
+        //document.querySelector('#search aside').classList.add('success')
+        document.querySelector('form.weather').reset();
+
+
+})
+})
